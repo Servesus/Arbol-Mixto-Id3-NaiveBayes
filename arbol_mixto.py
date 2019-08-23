329bd52521d01d69e2e66e2818269afe2644493c
Servesus <jesusgarrocho1998@gmail.com>
2019-08-23 08:11:03 +0200

    Añadidas funciones para cargar datos y calcular entropia

--- arbol_mixto.py
+++ arbol_mixto.py
@@ -1,27 +1,62 @@
 import networkx as nx
 from nodo import Nodo
-from random import randrange
+import pandas
+import math
+
 class ArbolMixto:
-    def __init__(self,atributos):
+    def __init__(self):
         """
         Nodos contiene todos los nodos del arbol en orden de forma que el nodo en la posicion 0 es el nodo raiz.
         Atributos contiene la lista de los headers de los datos, en caso de tener que ir modificando hacer una copia de esta lista.
         """
         self.nodos = []
-        self.atributos = atributos
+        self.atributos = []
 
     def convertir_arbol_grafo(self):
         """
         Convierte el arbol de un grafo usando NetworkX, asi se puede imprimir por pantalla.
         """
         G = nx.Graph()
-        G.add_node(str(self.nodos[0].nombre))
+        G.add_node(self.nodos[0])
         for nodo in self.nodos:
             for hijo in nodo.hijos:
-                G.add_node(str(hijo.nombre),attribute=randrange(1000))
-                G.add_edge(str(nodo.nombre),str(hijo.nombre),label=str(hijo.arista))
+                G.add_node(hijo)
+                G.add_edge(nodo,hijo.nombre,label=hijo.arista)
 
 
         return G
+
+    def get_datos(headers,ruta):
+        """
+        El csv con los datos debe tener en la ultima columna el atributo clasificador
+        Headers debe ser true si el csv contiene headers, false en otro caso
+        Para que funcione correctamente la ruta, meter csv en carpeta datasets y pasar la ruta como
+        "datasets/cars.csv" con las comillas y cambiar cars.csv por el nombre que sea
+        Devuelve los datos como dataframe
+        """
+        r = str(ruta)
+        if headers == False:
+            datos = pandas.read_csv(r, header=None)
+        if headers == True:
+            datos = pandas.read_csv(r)
+
+        return datos
+
+    def get_entropia(datos):
+        """
+        Recibe como parametro los datos como dataframe devuelve la entropia del dataset
+        """
+        columna = datos.iloc[:,0]  
+        valores = list(set(datos.iloc[:,0]))
+        p = 0
+        for i in datos.iloc[:,0]:
+            if i == valores[0]:
+                p = p + 1
+        n = len(columna) - p
+        division1 = p/len(columna)
+        division2 = n/len(columna)
+        entropia = - (division1 * math.log(division1,2)) - (division2 * math.log(division2,2))
+        return entropia
+
 
 
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from arbol_mixto import ArbolMixto as a
 from nodo import Nodo
-import networkx as nx
-import matplotlib.pyplot as plt
+"""
+Test para imprimir arbol como grafo
 
+#import networkx as nx
+#import matplotlib.pyplot as plt
 cielo = Nodo("Cielo")
 humedad = Nodo("Humedad","Soleado")
 positivoNublado = Nodo("+","Nublado")
@@ -16,9 +18,21 @@ cielo.hijos = [humedad,positivoNublado,viento]
 humedad.hijos = [negativoAlta,positivoNormal]
 viento.hijos = [negativoFuerte,positivoDebil]
 
-t = a([])
+t = a()
 
 t.nodos = [cielo,humedad,positivoNublado,viento,negativoAlta,positivoNormal,negativoFuerte,positivoDebil]
 G = t.convertir_arbol_grafo()
 nx.draw(G,with_labels=True)
 plt.show()
+
+"""
+
+"""
+Test para probar a cargar datos
+"""
+
+t = a()
+datos = a.get_datos(False,"datasets/cars.csv")
+print(datos.head(10))
+
+print(a.get_entropia(datos))
