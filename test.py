d244d15906a39f2e05272fde16dd7e071dadc419
Servesus <jesusgarrocho1998@gmail.com>
2019-09-04 23:09:37 +0200

    Tests y avances en el paper

--- test.py
+++ test.py
@@ -1,73 +1,51 @@
 from arbol_mixto import ArbolMixto as a
 from nodo import Nodo
-"""
-#Test para imprimir arbol como grafo
 
-#import networkx as nx
-#import matplotlib.pyplot as plt
-cielo = Nodo("Cielo",None)
-humedad = Nodo("Humedad",cielo,"Soleado")
-positivoNublado = Nodo("+",cielo,"Nublado")
-viento = Nodo("Viento",cielo,"Lluvia")
-negativoAlta = Nodo("-",humedad,"Alta")
-positivoNormal = Nodo("+",humedad,"Normal")
-negativoFuerte = Nodo("-",viento,"Fuerte")
-positivoDebil = Nodo("+",viento,"Debil")
-
-cielo.hijos = [humedad,positivoNublado,viento]
-humedad.hijos = [negativoAlta,positivoNormal]
-viento.hijos = [negativoFuerte,positivoDebil]
-
-t = a()
-
-t.nodos = [cielo,humedad,positivoNublado,viento,negativoAlta,positivoNormal,negativoFuerte,positivoDebil]
-t.dibuja_arbol()
-
-G = t.convertir_arbol_grafo()
-nx.draw(G,with_labels=True)
-plt.show()
-
-
-
-
-Test para probar a cargar datos y calcular entropia
-
-
-t = a()
-datos = t.get_datos(True,"datasets/ejemplo1.csv")
-#print(datos.head(10))
-
-print(t.get_entropia(datos))
-
-print(datos.iloc[0,0])
-print(datos.loc[0,"Cielo"])
-print(list(datos.columns))
-
-
-b = datos['Temperatura']
-list_atributos = list(set(b))
-nuevos_datos = datos.loc[datos["Temperatura"] == "alta"]
-print(nuevos_datos)
-print(len(nuevos_datos.index))
-
-print(t.get_entropia(datos))
-print(t.get_ganancia(datos, 'Humedad'))
-print(t.get_ganancia(datos, 'Viento'))
-print(t.get_ganancia(datos, 'Cielo'))
-print(t.get_ganancia(datos,"Temperatura"))
-
-t.atributos = list(datos.columns)
-del t.atributos[-1]
-print(datos.columns)
-print(datos[t.get_mejor_atributo(datos)])
-ultima_columna = datos.iloc[:,-1]
-print(list(ultima_columna.mode())[0])
 
 """
-
 t = a()
-t.entrenar(False,"datasets/kr-vs-kp-test.csv",5)
+t.entrenar(False,"datasets/car-data-train.csv",200)
 t.dibuja_arbol()
-res = t.clasificar(False,"datasets/kr-vs-kp-train.csv")
+res = t.clasificar(False,"datasets/car-data-test.csv")
 print(res)
 print(t.rendimiento())
+"""
+"""
+#Experimento 1
+
+res = []
+for i in range(1,7):
+    t = a()
+    t.entrenar(True,"datasets/ejemplo2.csv",i)
+    t.clasificar(True,"datasets/evaluacion2.csv")
+    rendimiento = t.rendimiento()
+    res.append(rendimiento)
+print(res)
+"""
+"""
+
+#Experimento2
+res = []
+res2 = []
+for i in range(1,40,2):
+    t = a()
+    t.entrenar(False,"datasets/car-data-train.csv",i)
+    t.clasificar(False,"datasets/car-data-test.csv")
+    rendimiento = t.rendimiento()
+    res.append(rendimiento)
+    res2.append(i)
+print(res2)
+print(res)
+"""
+#Experimento3
+res = []
+res2 = []
+for i in range(1,40,2):
+    t = a()
+    t.entrenar(False,"datasets/kr-vs-kp-train.csv",i)
+    t.clasificar(False,"datasets/kr-vs-kp-test.csv")
+    rendimiento = t.rendimiento()
+    res.append(rendimiento)
+    res2.append(i)
+print(res2)
+print(res)
